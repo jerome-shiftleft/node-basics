2ed80415ac1c41d917ba1f00fd5abff59f9f2db0
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-03 23:40:41 +0800

    progress

--- app.js
+++ app.js
@@ -22,11 +22,23 @@ const server = http.createServer((req, res) => {
   } // end of if (url === "/")
 
   if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+    })
+
     fs.writeFileSync('message.txt', 'DUMMY');    
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
-  }
+  } // end of if (url === "/message" && method === "POST")
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
@@ -34,6 +46,6 @@ const server = http.createServer((req, res) => {
   res.write("<body><h1>Hello from my Node.js Server</h1></body>");
   res.write("</html>");
   res.end();
-});
+}); // end of const server = http.createServer((req, res) => {
 
 server.listen(3000);
