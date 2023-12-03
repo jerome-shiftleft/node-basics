11fc93ec56577ec34203e46d9fbb6054fb55b433
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-03 21:55:25 +0800

    progress

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  //console.log(req);
+  console.log(req.url, req.method, req.headers);
+  //process.exit();
 });
 
 server.listen(3000);
