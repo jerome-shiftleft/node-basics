f2c23dba64b014a6b39287605ef9eeac0b964142
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-03 22:22:22 +0800

    progress

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  //console.log(req);
-  console.log(req.url, req.method, req.headers);
-  //process.exit();
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-  res.write('</html>');
+  const url = req.url;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(`
+      <body>
+        <form action="/message" method="POST">
+          <input type="text" name="message">
+          <button type="submit">Send</button
+          </form>
+      </body>
+    `);
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
   res.end();
 });
 
