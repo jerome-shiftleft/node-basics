099a261793e5ca1db03d2558375bba4bb08b7391
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-04 19:43:02 +0800

    progress

--- routes.js
+++ routes.js
@@ -51,6 +51,8 @@ const requestHandler = (req, res) => {
 
 }; // end of const requestHandler = ()
 
+// module.exports = requestHandler;
+
 // module.exports = {
 //   handler: requestHandler,
 //   someText: 'some hard coded text'
